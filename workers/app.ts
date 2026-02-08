6ecb89e3394d20e99deafaf077bf8f43d27bd135
Mladen Terzic <terza98@hotmail.rs>
2026-02-08 10:59:17 +0100

    feat: add configuration files for Shopify app and implement database setup in request handler

--- workers/app.ts
+++ workers/app.ts
@@ -1,4 +1,5 @@
 import { createRequestHandler } from "react-router";
+import { setupDb } from "../app/shopify.server";
 
 declare module "react-router" {
   export interface AppLoadContext {
@@ -11,13 +12,21 @@ declare module "react-router" {
 
 const requestHandler = createRequestHandler(
   () => import("virtual:react-router/server-build"),
-  import.meta.env.MODE
+  import.meta.env.MODE,
 );
 
 export default {
   async fetch(request, env, ctx) {
-    return requestHandler(request, {
-      cloudflare: { env, ctx },
-    });
+    try {
+      // Initialize DB on first request
+      setupDb(env);
+
+      return await requestHandler(request, {
+        cloudflare: { env, ctx },
+      });
+    } catch (error) {
+      console.log(error);
+      return new Response("An unexpected error occurred", { status: 500 });
+    }
   },
 } satisfies ExportedHandler<Env>;
